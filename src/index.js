625817a875675adf48a60425eb439b904dd5bffe
fartec0 <fausto.siqueira@gmail.com>
2022-03-05 20:02:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ http.createServer((req, res) => {
     res.write(answer());
   else if (path === '/pick')
     res.write(JSON.stringify(pick()));
-  else if (path === '/loaderio-6a783276f4259dff6328df708439e2bf')
-    res.write(`loaderio-6a783276f4259dff6328df708439e2bf`);
+  else if (path === '/loaderio-2bc3a09986d3d7ae3640b3fa3808459b')
+    res.write(`loaderio-2bc3a09986d3d7ae3640b3fa3808459b`);
   else
     res.write(`USAGE:
                /question - get a random white card
